625cf5430861b31a55058120b4c21677d96f6a24
Yashikvlada <yashikvlada@gmail.com>
2020-09-19 17:55:31 +0300

    fix picture library

---
+++ Test/TestDataTable/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TestDataTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1093, 34);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "select* from Authors";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1093, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "С  и  н  х  р  о  н  и  з  и  р  о  в  а  т  ь";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 395);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1093, 242);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(1093, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "F    i     l     l";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(1093, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "D   e   b   u   g";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(89, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(921, 167);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 206);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1093, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripTextBox1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 179);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1093, 27);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(25, 23);
+            this.toolStripMenuItem1.Text = "1";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 637);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+    }
+}
+
---
+++ Test/TestDataTable/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Linq;
+using System.Web.UI.WebControls;
+using System.Windows.Documents;
+using System.Diagnostics;
+using System.IO;
+using Image = System.Drawing.Image;
+using System.Drawing.Imaging;
+
+namespace TestDataTable
+{
+    public partial class Form1 : Form
+    {
+       
+        DataSet set;
+        SqlDataAdapter da;
+        SqlCommandBuilder cmd;
+        string FilName;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ConnectionString = ConfigurationManager.ConnectionStrings["LibraryConnection"].ConnectionString;
+        static SqlConnection coon = new SqlConnection()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["LibraryConnection"].ConnectionString
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(ConnectionString);
+                set = new DataSet();
+
+                da = new SqlDataAdapter(textBox1.Text, conn);
+                
+                var cmd = new SqlCommandBuilder(da);
+                //SqlCommand updDateCmd = new SqlCommand("Update Authors set LastName = 'Look', FirstName = 'Tuul' where id = @pId", conn);
+                //updDateCmd.Parameters.Add(new SqlParameter("@pId", SqlDbType.Int));
+                //updDateCmd.Parameters["@pId"].SourceVersion = DataRowVersion.Original;
+                //updDateCmd.Parameters["@pId"].SourceColumn = "id";
+                //da.UpdateCommand = updDateCmd;
+
+                SqlCommand updatesqlCommand = new SqlCommand("UpdateAuthors", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                SqlParameterCollection parameter = updatesqlCommand.Parameters;
+                parameter.Add("@pid", SqlDbType.Int, 0, "id");
+                parameter["@pid"].SourceVersion = DataRowVersion.Original;
+                   parameter.Add("@pLastName", SqlDbType.VarChar, 0, "LastName");
+                parameter.Add("@pFirstName", SqlDbType.VarChar, 0, "FirstName");
+
+
+                da.Fill(set, "mybook");
+                dataGridView1.DataSource = set.Tables["mybook"];
+
+            } 
+            catch (Exception exp)
+            {
+               MessageBox.Show(exp.ToString());
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            da.Update(set, "mybook");
+            //Debug.WriteLine(cmd.GetInsertCommand().CommandText);
+          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog { };
+                coon.Open();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FilName = openFileDialog.FileName;
+                    var bytes = CreatCoppy(FilName);
+                    if ((toolStripTextBox1.Text?.Length ?? 0) != 0 && int.TryParse(toolStripTextBox1.Text, out int index))
+                    {
+                        var comm = new SqlCommand("Insert into Pictures (Bookid, Name,Picture) values (@Bookid, @Name,@Picture)", coon);
+                        comm.Parameters.AddWithValue("@Bookid", index);
+                        comm.Parameters.AddWithValue("@Name", Path.GetFileName(Name));
+                        comm.Parameters.AddWithValue("@Picture", bytes);
+                        comm.ExecuteNonQuery();
+                        MessageBox.Show("srtrw");
+                    }
+                }
+                coon.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+
+        }
+
+        private object CreatCoppy(string filName)
+        {
+            var img = Image.FromFile(filName);
+            int maxWidht = 300;
+            int maxHidht = 300;
+            int newwidht = (int)(img.Width * (double)maxWidht / img.Width);
+            int newHidht = (int)(img.Height * (double)maxHidht / img.Height);
+
+            var imageRation = new Bitmap(newwidht, newHidht);
+            var g = Graphics.FromImage(imageRation);
+            g.DrawImage(img, 0, 0, newwidht, newHidht);
+
+            using (var stream = new MemoryStream()) 
+            using (var reader = new BinaryReader(stream) )
+            {
+                imageRation.Save(stream, ImageFormat.Jpeg);
+                stream.Flush();
+                stream.Seek(0, SeekOrigin.Begin);
+                return reader.ReadBytes((int)stream.Length);
+            }
+        }
+    
+        
+    }
+}
